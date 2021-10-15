453a9bcbebf5c9f66f779de3ce5fdebe39aeeb17
lianlian <kuliantnt@gmail.con>
2021-10-16 00:20:46 +0800

    格式化

--- src/main.go
+++ src/main.go
@@ -15,11 +15,15 @@ import (
 	"strings"
 )
 
+//init 初始化日志导出
 func init() {
+	//日期格式化
 	customFormatter := new(logrus.TextFormatter)
 	customFormatter.DisableQuote = true
 	customFormatter.TimestampFormat = "2006-01-02 15:04:05.000"
+	//日期格式
 	logrus.SetFormatter(customFormatter)
+	//设置输出
 	logrus.SetOutput(&lumberjack.Logger{
 		Filename: "ddl.log",
 		MaxSize:  500, //M
@@ -40,19 +44,24 @@ func getPos(host string, port uint64, username, password string) mysql.Position 
 }
 
 func main() {
-	cfgserver, err := ini.Load("config.ini")
+	//ini日志读取
+	cfgServer, err := ini.Load("config.ini")
 	if err != nil {
 		fmt.Println("文件读取错误", err)
 		os.Exit(1)
 	}
 	var serverId uint64
-	flavor := cfgserver.Section("server").Key("flavor").Value()
-	serverId, _ = cfgserver.Section("server").Key("serverID").Uint64()
-	host := cfgserver.Section("server").Key("host").Value()
-	port, _ := cfgserver.Section("server").Key("port").Uint64()
-	username := cfgserver.Section("server").Key("username").Value()
-	password := cfgserver.Section("server").Key("password").Value()
+	//如果数据库类型为mysql则输入mysql,如果为mariadb则输入mariadb
+	flavor := cfgServer.Section("server").Key("flavor").Value()
+	//从库的server_id,和被监控的库的server_id不能相同
+	serverId, _ = cfgServer.Section("server").Key("serverID").Uint64()
+	//链接信息
+	host := cfgServer.Section("server").Key("host").Value()
+	port, _ := cfgServer.Section("server").Key("port").Uint64()
+	username := cfgServer.Section("server").Key("username").Value()
+	password := cfgServer.Section("server").Key("password").Value()
 
+	//mysql同步配置
 	cfg := replication.BinlogSyncerConfig{
 		ServerID: uint32(serverId),
 		Flavor:   flavor,
@@ -61,48 +70,52 @@ func main() {
 		User:     username,
 		Password: password,
 	}
+	//mysql新日志同步
 	syncer := replication.NewBinlogSyncer(cfg)
-
+	//获取位置
 	pos := getPos(host, port, username, password)
+	//开始同步
 	streamer, _ := syncer.StartSync(pos)
-	var checksumAlgorithm byte=0
-	var first_Rotate_Event bool = true
+	//校验和算法
+	var checkSumAlgorithm byte = 0
+	//第一个Rotate事件
+	var firstRotateEvent = true
 	for {
 		ev, _ := streamer.GetEvent(context.Background())
 		event := ev.Header.EventType
 
 		// ev.Dump(os.Stdout)
-		if event == replication.FORMAT_DESCRIPTION_EVENT{
+		if event == replication.FORMAT_DESCRIPTION_EVENT {
 			fde := DecodeFDEvent(ev.RawData)
-			checksumAlgorithm=fde.ChecksumAlgorithm
+			checkSumAlgorithm = fde.ChecksumAlgorithm
 		}
 
-		if event == replication.ROTATE_EVENT{
+		if event == replication.ROTATE_EVENT {
 			var rotateEvent replication.RotateEvent
-			if first_Rotate_Event == true{
-				rotateEvent = DecodeRotate(checksumAlgorithm,ev.RawData)
-			}else{
-				rotateEvent = DecodeRotate(checksumAlgorithm,ev.RawData)
+			if firstRotateEvent == true {
+				rotateEvent = DecodeRotate(checkSumAlgorithm, ev.RawData)
+			} else {
+				rotateEvent = DecodeRotate(checkSumAlgorithm, ev.RawData)
 			}
 
-			logrus.Infof("rotate to (%s, %d)",rotateEvent.NextLogName,rotateEvent.Position)
-			log.Printf("rotate to (%s, %d)\n",rotateEvent.NextLogName,rotateEvent.Position)
+			logrus.Infof("rotate to (%s, %d)", rotateEvent.NextLogName, rotateEvent.Position)
+			log.Printf("rotate to (%s, %d)\n", rotateEvent.NextLogName, rotateEvent.Position)
 		}
-		first_Rotate_Event = false
+		firstRotateEvent = false
 		if event == replication.QUERY_EVENT {
 			header := DecodeHeader(ev.RawData)
 			if header.Flags == 8 {
 				continue
 			}
-			qe := DecodeBody(checksumAlgorithm,ev.RawData)
+			qe := DecodeBody(checkSumAlgorithm, ev.RawData)
 			query := string(qe.Query)
 
 			//过滤一些DDL
 			if strings.TrimSpace(strings.ToUpper(query)) == "FLUSH PRIVILEGES" {
 				continue
 			}
-			logrus.Info(getMsg(checksumAlgorithm,ev.RawData))
-			log.Printf(getMsg(checksumAlgorithm,ev.RawData))
+			logrus.Info(getMsg(checkSumAlgorithm, ev.RawData))
+			log.Printf(getMsg(checkSumAlgorithm, ev.RawData))
 		}
 	}
 
